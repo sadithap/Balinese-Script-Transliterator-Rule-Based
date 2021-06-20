497c9089bae60d92c0ee3a43f27085ce58f847ef
I Gede Ketut Saditha Putra <sadithaputra5@gmail.com>
2021-06-20 21:52:38 +0800

    update python script

--- app/src/main/python/filter4.py
+++ app/src/main/python/filter4.py
@@ -7,9 +7,7 @@ wordcost = dict((k, log((i+1)*log(len(words)))) for i,k in enumerate(words))
 maxword = max(len(x) for x in words)
 
 def infer_spaces(s):
-    # Find the best match for the i first characters, assuming cost has
-    # been built for the i-1 first characters.
-    # Returns a pair (match_cost, match_length).
+
     def best_match(i):
         candidates = enumerate(reversed(cost[max(0, i-maxword):i]))
         return min((c + wordcost.get(s[i-k-1:i], 9e999), k+1) for k,c in candidates)
